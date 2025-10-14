4d520ea7fc5023f7c961e7edabfd0a9f3ad79b6e
Premchand <premsgowda25@gmail.com>
2025-10-14 15:12:31 +0530

    updated code to connect with DMR(Docker Model Runner) AI instead of locally downloaded ollama or openAI

--- src/main/java/com/example/SpringAi_Demo/controller/chatController.java
+++ src/main/java/com/example/SpringAi_Demo/controller/chatController.java
@@ -1,7 +1,7 @@
 package com.example.SpringAi_Demo.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.ollama.OllamaChatModel;
+//import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
